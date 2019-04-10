41a8269e8994af4302ba17a3c0df5a637d1194f2
LimboDemon <wang_zy2412@163.com>
2019-04-10 10:24:41 +0800

    提交测试代码

---
+++ LDTest_D/Test/Test.h
@@ -0,0 +1,18 @@
+//
+//  Test.h
+//  LDTest_D
+//
+//  Created by 王朝阳 on 2019/4/10.
+//  Copyright © 2019 LimboDemon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LDTest_D/Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  LDTest_D
+//
+//  Created by 王朝阳 on 2019/4/10.
+//  Copyright © 2019 LimboDemon. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
